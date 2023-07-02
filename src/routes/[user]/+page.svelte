6299708486632606195f8383b7c854c3c733e79c
xKairu <kairu@team-reflex.com>
2023-07-03 11:02:06 +1200

    pulse progress bar on initial load

--- src/routes/[user]/+page.svelte
+++ src/routes/[user]/+page.svelte
@@ -44,7 +44,12 @@
 <div class="w-full flex flex-col gap-6">
   {#if loading}
     <div class="flex flex-col gap-1 justify-center items-center py-48">
-      <div class="w-3/4 md:w-1/3 bg-accent dark:bg-foreground h-1 shadow-sm rounded">
+      <div
+        class={cn(
+          'w-3/4 md:w-1/3 bg-accent dark:bg-foreground h-1 shadow-sm rounded',
+          totalCount === 0 && 'animate-pulse'
+        )}
+      >
         <div
           class={cn('bg-blue-500 h-1 rounded transition-all duration-300', error && 'bg-red-500')}
           style:width={`${percentage}%`}
