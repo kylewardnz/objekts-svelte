d7d5d2db3f1ca07ecd150e787854bcc04c19ce96
xKairu <kairu@team-reflex.com>
2023-07-27 11:54:05 +1200

    styling

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,6 +5,10 @@
   import About from '$components/about.svelte'
   import { PUBLIC_UMAMI_ANALYTICS } from '$env/static/public'
   import { TooltipProvider } from '$components/ui/tooltip'
+  import { page } from '$app/stores'
+  import { cn } from '$lib/utils'
+
+  $: isIndex = $page.url.pathname === '/'
 </script>
 
 <svelte:head>
@@ -19,16 +23,18 @@
   {/if}
 </svelte:head>
 
-<div class="h-screen container flex flex-col mx-auto gap-6 p-5">
+<div class="h-[100dvh] container flex flex-col mx-auto gap-6 p-5">
   <!-- nav -->
-  <div class="grid grid-cols-3 items-center">
+  <div class="grid grid-cols-3 items-center h-8">
     <div class="flex justify-start">
       <Theme />
     </div>
     <a
       href="/"
-      class="mx-auto w-fit text-center text-lg underline underline-offset-8 decoration-blue-500"
-      >objekts</a
+      class={cn(
+        'mx-auto w-fit text-center underline underline-offset-8 decoration-blue-500 transition-all',
+        isIndex ? 'text-2xl' : 'text-lg'
+      )}>objekts</a
     >
     <div class="flex justify-end">
       <About />
