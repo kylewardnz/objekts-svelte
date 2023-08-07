8e425fe4061896daf0a12ae4925e729f98f31f48
xKairu <kairu@team-reflex.com>
2023-08-08 01:33:59 +1200

    use edge for everything except nft fetch

--- src/routes/api/objekts/[user]/+server.ts
+++ src/routes/api/objekts/[user]/+server.ts
@@ -2,6 +2,11 @@ import { error, json } from '@sveltejs/kit'
 import type { RequestHandler } from './$types'
 import { fetchByPage } from '$lib/server/nft'
 import { isAddress } from '$lib/utils'
+import type { Config } from '@sveltejs/adapter-vercel'
+
+export const config: Config = {
+  runtime: 'nodejs18.x'
+}
 
 export const GET = (async ({ params, url }) => {
   if (!isAddress(params.user)) {
