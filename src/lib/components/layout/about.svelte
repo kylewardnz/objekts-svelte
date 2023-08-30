e912c24e43a584ef0ee9440ad06d1803479b13f5
xKairu <kairu@team-reflex.com>
2023-08-30 17:55:32 +1200

    add cosmo links

--- src/lib/components/layout/about.svelte
+++ src/lib/components/layout/about.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
   import * as AlertDialog from '$components/ui/alert-dialog'
+  import Button from '$components/ui/button/button.svelte'
   import { HelpCircle } from 'lucide-svelte'
 </script>
 
@@ -10,8 +11,25 @@
   <AlertDialog.Content>
     <AlertDialog.Header>
       <AlertDialog.Title>About</AlertDialog.Title>
-      <AlertDialog.Description>
+      <AlertDialog.Description class="flex flex-col gap-4">
         <p>Not associated with Modhaus, Cosmo, tripleS, ARTMS or ODD EYE CIRCLE.</p>
+        <p>
+          Download Cosmo from the <a
+            target="_blank"
+            class="underline hover:text-primary transition"
+            href="https://apps.apple.com/app/cosmo-the-gate/id1617337069"
+          >
+            App Store
+          </a>
+          or
+          <a
+            target="_blank"
+            class="underline hover:text-primary transition"
+            href="https://play.google.com/store/apps/details?id=com.modhaus.cosmo"
+          >
+            Google Play
+          </a>
+        </p>
       </AlertDialog.Description>
     </AlertDialog.Header>
     <AlertDialog.Footer>
