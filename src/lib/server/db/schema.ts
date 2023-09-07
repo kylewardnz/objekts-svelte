423868ba3e2281102a4590a137e50200448a45ac
xKairu <kairu@team-reflex.com>
2023-09-08 03:57:33 +1200

    add primary key to users table

--- src/lib/server/db/schema.ts
+++ src/lib/server/db/schema.ts
@@ -1,11 +1,17 @@
 import type { InferInsertModel, InferSelectModel } from 'drizzle-orm'
-import { boolean, mysqlTable, varchar } from 'drizzle-orm/mysql-core'
+import { boolean, index, mysqlTable, serial, varchar } from 'drizzle-orm/mysql-core'
 
-export const users = mysqlTable('users', {
-  // cosmo nicknames must be unique, so we can use it as a primary key
-  cosmoNickname: varchar('cosmo_nickname', { length: 32 }).primaryKey(),
-  polygonAddress: varchar('polygon_address', { length: 255 }).notNull(),
-  isPrivate: boolean('is_private').notNull().default(false)
-})
+export const users = mysqlTable(
+  'users',
+  {
+    id: serial('id').primaryKey(),
+    cosmoNickname: varchar('cosmo_nickname', { length: 32 }).notNull().unique(),
+    polygonAddress: varchar('polygon_address', { length: 255 }).notNull(),
+    isPrivate: boolean('is_private').notNull().default(false)
+  },
+  (table) => ({
+    cosmoNicknameIdx: index('cosmo_nickname_idx').on(table.cosmoNickname)
+  })
+)
 export type User = InferSelectModel<typeof users>
 export type InsertUser = InferInsertModel<typeof users>
